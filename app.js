c9f2baca4f49334ff269ff0748178af59fb5d19e
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2016-04-05 14:44:15 -0400

    Angular

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var app1 = angular.module('app1', []);
+
 lista = [{name: 'bon', source: 'bon.jpg', count:0}, {name: 'hon',
 source: 'hon.jpg', count:0},{name: 'jojo', source: 'jojo.jpg', count:0},
 {name: 'jon', source: 'jon.jpg', count:0},{name: 'koko', source: 'koko.jpg',
 count:0},{name: 'lon', source: 'lon.jpg', count:0},{name: 'mon', 
 source: 'mon.jpg', count:0},{name: 'toto', source: 'toto.jpg', count:0},];
+
 app1.controller('iterata', function($scope){
 	$scope.list= lista;
 	$scope.updateValue = function(mama){
 		$scope.conta = mama.count++;
 	}
 });
+
 
